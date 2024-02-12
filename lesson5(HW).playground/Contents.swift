651f56678f786b2ca12499d37d129c10da7e854c
Max <ilyakostuc@gmail.com>
2024-02-12 12:56:16 +0200

    change(1)

--- lesson5(HW).playground/Contents.swift
+++ lesson5(HW).playground/Contents.swift
@@ -1,7 +1,32 @@
 import Cocoa
 
-//MARK: Enum - Optional
-enum Cars {
-    case Tesla
-    case Toyota
+//MARK: (2)
+enum Seasons: String {
+    case winter = "Зима"
+    case spring = "Весна"
+    case summer = "Лето"
+    case autumn = "Осень"
 }
+
+func numberMonth(_ number: Int) -> String {
+    var season = ""
+    
+    switch number {
+    case 1...2, 12:
+        season = Seasons.winter.rawValue
+    case 3...5:
+        season = Seasons.spring.rawValue
+    case 6...8:
+        season = Seasons.summer.rawValue
+    case 9...11:
+        season = Seasons.autumn.rawValue
+    default:
+        break
+    }
+    return season
+}
+let season = numberMonth(3)
+print(season)
+
+
+
